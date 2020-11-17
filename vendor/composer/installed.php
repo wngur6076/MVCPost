9e8d7c55ca7b8b9461a99eb5b58c0f11ef123f45
wngur6076 <wngur6076@naver.com>
2020-11-18 04:59:02 +0900

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '143e0d3d763cb8e1663dde912a95cabaf4e806cf',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '143e0d3d763cb8e1663dde912a95cabaf4e806cf',
+    ),
+    'pronist/eclair' => 
+    array (
+      'pretty_version' => '0.0.1',
+      'version' => '0.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '244dae682cec7d2a6ebe9da1f43d729ac700cf98',
+    ),
+  ),
+);
